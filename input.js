5c4211f61779cb9e13d40011cae41d4594ece03e
snowystuff <ruler-beeper.0z@icloud.com>
2025-07-02 13:23:58 -0700

    new project frame testing
    debug version of the "New Project" frame pops up when you click the button

---
+++ input.js
@@ -0,0 +1,39 @@
+i_ok = document.getElementById("input_ok");
+i_cancel = document.getElementById("input_cancel");
+
+test_new = document.getElementById("new_project");
+
+bg = document.getElementById("frame_background");
+
+inpfall = document.getElementsByClassName("input_frame")[0];
+inpfen = false;
+
+function updateFrame(bool,send) {
+	if (bool === true || bool === false) {
+		inpfen = bool;
+	} else {
+		inpfen = !inpfen;
+	}
+	
+	if (inpfen === false) {
+		bg.style.display = "none";
+		inpfall.style.display = "none";
+	} else {
+		bg.style.display = "unset";
+		inpfall.style.display = "unset";
+	}
+}
+
+i_ok.addEventListener('click', () => {
+	updateFrame(false,true);
+});
+
+i_cancel.addEventListener('click', () => {
+	updateFrame(false,false);
+});
+
+test_new.addEventListener('click', () => {
+	updateFrame(true);
+});
+
+updateFrame(false);
